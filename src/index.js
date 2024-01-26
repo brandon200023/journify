12d1dfc6b071d4a1dcd1edefe2ac3bb676f20ceb
brandon200023 <brandonkiiza18@gmail.com>
2024-01-26 12:07:33 +0300

    designed the layout of the page and added components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Layout from './components/Layout';
+import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
+import SinglePost from './pages/SinglePost.jsx';
+import Dashboard from './components/Dashboard.jsx';
+import Register from './pages/Register.jsx';
+import Login from './pages/Login.jsx';
+import UserProfile from './pages/UserProfile.jsx';
+import CreatePost from './pages/CreatePost.jsx';
+import CatergoryPost from './pages/CatergoryPost.jsx';
+import EditPost from './pages/EditPost.jsx';
+import Logout from './pages/Logout.jsx';
+
+const router =createBrowserRouter([
+  {
+    path:"/",
+    element:<Layout/>,
+    errorElement:<ErrorPage/>,
+    children:[
+      {index:true, element: <Home/>},
+      {path:"posts/:id", element: <SinglePost />},
+      {path:"register", element: <Register />},
+      {path:"login", element: <Login />},
+      {path:"profile/id", element: <UserProfile />},
+      {path:"create", element: <CreatePost />},
+      {path:"posts/categories/:category", element: <CatergoryPost/>},
+      {path:"myposts/:id", element: <Dashboard />},
+      {path:"posts/:id/edit", element: <EditPost />},
+      {path:"logout", element: <Logout />}
+
+
+      
+    ]
+    
+  }
+
+])
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
+    
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/CatergoryPost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CatergoryPost() {
+  return (
+    <div>CatergoryPost</div>
+  )
+}
+
+export default CatergoryPost
